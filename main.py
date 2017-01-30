32537abfa27f84024a599abe5473fa54529d49f2
Patrick Dassier <patrick.dassier@legrand.fr>
2017-01-30 18:54:20 +0100

    developping UI

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ Created on 27 janv. 2017
 '''
 
 import kivy
+from kivy.uix.boxlayout import BoxLayout
 kivy.require('1.9.1')
 
 from kivy.uix.slider import Slider
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.properties import BooleanProperty
 from kivy.properties import NumericProperty
+from kivy.properties import StringProperty
 from kivy.uix.button import Button
 
 
@@ -34,17 +37,31 @@ class TouchPanelSlider(Slider):
     activate = BooleanProperty(True)
     
 
+class TouchPanelMain(BoxLayout):
+    '''
+    Main windows
+    '''
+    app = ObjectProperty(None)
+    menu_layout = ObjectProperty(None)
+    button_layout = ObjectProperty(None)
+    slider_layout = ObjectProperty(None)
+    
+    def __init__(self, **kwargs):
+        super(TouchPanelMain, self).__init__(**kwargs)
+        
+        for i in range(6):
+            btn = TouchPanelButton(index=i)
+            self.button_layout.add_widget(btn)
+
+        for i in range(6):
+            sld = TouchPanelSlider(index=i)
+            self.slider_layout.add_widget(sld)
+            
+    
 class TouchPanelApp(App):
     def build(self):
-        layout = GridLayout(cols=6)
-        for i in range(6):
-            sld = TouchPanelSlider(index=i, orientation='vertical', min=0, max=255)
-            layout.add_widget(sld, index=i)
-        #for i in range(9):
-        #    btn = TouchPanelButton(text="{}".format(i), index=i)
-        #    layout.add_widget(btn, index=i)
-        return layout
-        
+        return TouchPanelMain(app=self)
+
 
 if __name__ == '__main__':
     TouchPanelApp().run()
